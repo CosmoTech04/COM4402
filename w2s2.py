534f0d4bcbe0ada102fd3debe27da0a7e7da97dc
cp5crt <cosmin13ciobanu@gmail.com>
2025-12-16 11:10:22 +0000

    w2s1

---
+++ w2s2.py
@@ -0,0 +1,14 @@
+#activity 8
+name = str(input("Enter your name:"))
+age = int(input("Enter age:"))
+height = float(input("Enter height"))
+age_plus_one = age + 1
+print("Hello")
+print("Next year you will be", age_plus_one)
+print("Your Height is", height, "metres")
+
+#activity 10
+name = str(input("Enter your name: "))
+age = int(input("Enter your age :"))
+age_next_year = age + 1
+print("Hello", name, "next year you will be", age_next_year)
