b3cc05b054aa778748fd6ad3d789a70e14811185
cp5crt <cosmin13ciobanu@gmail.com>
2025-12-16 10:23:36 +0000

    w2s1

--- w2s1.py
+++ w2s1.py
@@ -2,3 +2,16 @@
 name = input("Enter your name:")
 age = input("Enter your age")
 print("Hello", name, "you are", age, "age old")
+
+#activity 2
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+print("sum is",str(a + b), "and Product is", str(a * b))
+
+#activity 6
+name = str(input("Enter Product Name:"))
+price = float(input("Enter Price:"))
+quantity = int(input("Enter Quantity:"))
+total_cost = price * quantity
+print("You bought", quantity, "of", name)
+print("Total Cost is", total_cost)
