fb82b53f783a35baad456aae3f57d20827383f3f
cp5crt <cosmin13ciobanu@gmail.com>
2025-12-18 09:25:21 +0000

    w3s1

---
+++ if loops.py
@@ -0,0 +1,7 @@
+score = int(input("Please enter score"))
+if score >= 70:
+    print("Distinction")
+elif score >= 40:
+    print("pass")
+else:
+    print("fail")
--- w2s2.py
+++ w2s2.py
@@ -1,14 +1,3 @@
-#activity 8
-name = str(input("Enter your name:"))
-age = int(input("Enter age:"))
-height = float(input("Enter height"))
-age_plus_one = age + 1
-print("Hello")
-print("Next year you will be", age_plus_one)
-print("Your Height is", height, "metres")
-
-#activity 10
-name = str(input("Enter your name: "))
-age = int(input("Enter your age: "))
-age_next_year = age + 1
-print("Hello", name, "next year you will be", age_next_year)
+full_name = str(input("What is your name:")).title()
+course_name = str(input("Enter course name")).upper()
+print(f"Student: {full_name} ({course_name})")
