0b7147e3707c377154ff0c9931fae1b603f990ad
cp5crt <cosmin13ciobanu@gmail.com>
2026-01-06 15:16:28 +0000

    w3s1

---
+++ w3s2.py
@@ -0,0 +1,39 @@
+# problem 1
+# number = int(input("gimme a number u sucker: "))
+# while number > 1:
+#     number = number - 1
+#     print(number)
+# print("lift off")
+#  problem 2
+# n = int(input("gimme a number u sucker: "))
+# total = 0
+# while True:
+#     total = total + n
+#     n = int(input("and another one: "))
+#     if n == 0:
+#         break
+# print("your total is: ", total)
+# problem 3
+# correct_password = "python123"
+#
+# while True:
+#
+#     password = input(("enter the password: ")).lower()
+#     if password == correct_password:
+#         break
+#     else:
+#         print(("try again"))
+# print("Access granted")
+# problem 4
+# secret = 17
+# guess = 0
+# while guess != secret:
+#     guess = int(input("enter guess u incell: "))
+#     if guess > secret:
+#         print("Too high")
+#     elif guess < secret:
+#         print("too low")
+#     else:
+#         break
+# print("Well done u incell")
+# problem 5
